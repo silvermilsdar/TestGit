8a61e19173cd3388395514fc5f8f232dbd77efd8
silvermilsdar <elmagy0078@gmail.com>
2022-12-19 15:26:06 +0400

    Initial Commit

---
+++ TestGit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestGit
+//
+//  Created by Магомед Рамазанов on 19.12.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
